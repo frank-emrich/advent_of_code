f5e1426a591d21ef624bfbcb478d6cf7055c53a0
Frank Emrich <git@emrich.io>
2019-12-01 20:57:02 +0000

    day 1 2019

---
+++ 2019/Aoc2019.ml
@@ -0,0 +1,25 @@
+
+
+module type AOC_SOLVER =
+sig
+  val solve : unit -> string list
+end
+
+
+let print_solutions =
+  let modules : (module AOC_SOLVER) list=
+    [(module Day1)]
+  in
+  let print_solution_parts day_z_based solver_module =
+    let module Solver = (val solver_module : AOC_SOLVER) in
+    let results = Solver.solve () in
+    let print_result part_z_based part_result =
+      Printf.printf
+        "result part %d for day %d is:\n%s\n"
+        (part_z_based + 1)
+        (day_z_based + 1)
+        part_result
+    in
+    List.iteri print_result results
+  in
+  List.iteri print_solution_parts modules
---
+++ 2019/Aoc_utils.ml
@@ -0,0 +1,11 @@
+let read_file path =
+  let chan = open_in path in
+  let rec add_line prev_lines =
+    try
+      let next_line = input_line chan in
+      add_line (next_line :: prev_lines)
+    with
+    | End_of_file ->
+       prev_lines
+  in
+  add_line [] |> List.rev
