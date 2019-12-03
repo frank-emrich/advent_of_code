db0ee5bfd90c2be64e79f7c93b4e8dacfa255300
Frank Emrich <git@emrich.io>
2019-12-03 15:03:55 +0000

    cleand up day 1

--- 2019/1/day1.ml
+++ 2019/1/day1.ml
@@ -7,23 +7,16 @@ let compute_fuel mass =
 
 
 let rec compute_fuel_with_own_weight mass =
-  let mass_float = float_of_int mass in
-  let minus_two a = a -. 2.0 in
-  let fuel_needed_no_own_weight =
-    mass_float /. 3.0 |> floor |> minus_two |> int_of_float
-  in
+  let fuel_needed_no_own_weight = compute_fuel mass in
   if fuel_needed_no_own_weight < 0 then
     0
   else
     fuel_needed_no_own_weight
     + compute_fuel_with_own_weight fuel_needed_no_own_weight
 
-
-
 let solve () =
   let data_str = Aoc_utils.read_file "1/data.txt" in
   let data_int = List.map int_of_string data_str in
-
 
   let result_part_one =
     List.fold_left
