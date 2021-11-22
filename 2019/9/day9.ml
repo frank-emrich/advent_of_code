354ea6df20b900298f37c4380fe27e71a7cc703e
Frank Emrich <git@emrich.io>
2021-11-22 16:19:42 +0000

    abstract over state impl

--- 2019/9/day9.ml
+++ 2019/9/day9.ml
@@ -2,151 +2,229 @@ open! Core
 module IntMap = Core.Int.Map
 (* module Z = Zarith.Z *)
 
-type state = {
+module type State =
+sig
+  type t
+
+
+  val output : t -> Z.t list
+  val relative_base : t -> int
+  val pc : t -> int
+  val is_final : t -> bool
+
+  val with_pc : t -> int -> t
+  val with_relative_base : t -> int -> t
+  val pop_input_exn : t -> t * Z.t
+  val push_output : t -> Z.t -> t
+
+  val read_memory : t -> int -> Z.t
+  val write_memory : t -> int -> Z.t -> t
+
+  val stop : t -> t
+
+  val create_initial : input:(Z.t list) -> initial_data:(Z.t list) -> t
+
+end
+
+
+module Common_state =
+struct
+  type 'a state =  {
   input : Z.t list;
   output : Z.t list;
-  data : Z.t IntMap.t;
+  data : 'a;
   relative_base : int;
   stop : bool;
   index : int;
 }
 
-let make_op_fun (op_code : int) : state -> (Z.t * int) array -> state =
-  let inc_index i args = i + Array.length args + 1 in
-  let mk_arith op state args =
-    let result = op (fst args.(0)) (fst args.(1)) in
-    let update_index = snd args.(2) in
-    let new_data = IntMap.set state.data ~key:update_index ~data:result in
-    { state with data = new_data; index = inc_index state.index args }
-  in
-  let mk_cond_jmp (op : Z.t -> bool) state args =
-    if op (fst args.(0)) then
-      let alt_value = fst args.(1) |> Z.to_int in
-      { state with index = alt_value }
-    else { state with index = inc_index state.index args }
-  in
-  let mk_comp op state args =
-    let result = if op (fst args.(0)) (fst args.(1)) then Z.one else Z.zero in
-    let new_data = IntMap.set state.data ~key:(snd args.(2)) ~data:result in
-    { state with data = new_data; index = inc_index state.index args }
-  in
-  let mk_adjust_relative_base state args =
-    {
-      state with
-      relative_base = state.relative_base + (fst args.(0) |> Z.to_int);
-      index = inc_index state.index args;
-    }
-  in
-  match op_code with
-  | 1 -> mk_arith Z.( + )
-  | 2 -> mk_arith Z.( * )
-  | 3 ->
-      fun state args ->
-        let next_input = List.hd_exn state.input in
-        let update_index = snd args.(0) in
-        let new_data =
-          IntMap.set state.data ~key:update_index ~data:next_input
-        in
-        {
-          state with
-          data = new_data;
-          input = List.tl_exn state.input;
-          index = inc_index state.index args;
-        }
-  | 4 ->
-      fun state args ->
-        {
-          state with
-          output = fst args.(0) :: state.output;
-          index = inc_index state.index args;
-        }
-  | 5 -> mk_cond_jmp (Z.Compare.( <> ) Z.zero)
-  | 6 -> mk_cond_jmp (Z.Compare.( = ) Z.zero)
-  | 7 -> mk_comp Z.Compare.( < )
-  | 8 -> mk_comp Z.Compare.( = )
-  | 9 -> mk_adjust_relative_base
-  | 99 -> fun state _args -> { state with stop = true }
-  | bad -> failwith @@ Printf.sprintf "unexpected op_code: %d\n" bad
+  let pop_input_exn s = {s with input = List.tl_exn s.input}, List.hd_exn s.input
+  let output s = s.output
+  let relative_base s = s.relative_base
+  let pc s = s.index
+  let is_final s = s.stop
 
-let parse_op_spec op_spec =
-  let param_count_of_op = function
-    | 99 -> 0
-    | 3 | 4 -> 1
-    | 5 | 6 -> 2
-    | 1 | 2 | 7 | 8 -> 3
-    | 9 -> 1
-    | _ -> failwith "illegal opcode"
-  in
-  let rec parse_param_mode count code =
-    if count = 0 then []
-    else
-      let rem = code mod 10 in
-      let mode =
-        match rem with
-        | 2 -> `Relative
-        | 1 -> `Immediate
-        | 0 -> `Parameter
-        | _ -> failwith "unexpected parameter mode"
-      in
-      let next = code / 10 in
-      mode :: parse_param_mode (count - 1) next
-  in
-  let op_code = op_spec mod 100 in
-  let param_spec = op_spec / 100 in
-  let param_count = param_count_of_op op_code in
-  let param_spec = parse_param_mode param_count param_spec in
-  (op_code, param_count, param_spec)
+  let with_pc s pc = {s with index = pc}
+  let with_relative_base s relative_base  = {s with relative_base}
 
-let read_memory state address =
-  Option.value ~default:Z.zero (IntMap.find state.data address)
+  let push_output s new_out = {s with output = new_out :: s.output }
 
-let fetch_args state arg_index param_count param_specs =
-  let args = Array.create ~len:param_count (Z.zero, 0) in
-  let fetch index = function
-    | `Immediate ->
-        let v = read_memory state (arg_index + index) in
-        args.(index) <- (v, v |> Z.to_int)
-    | `Parameter ->
-        let indirect_index = Z.to_int (read_memory state (arg_index + index)) in
-        let indirect_result = read_memory state indirect_index in
-        (* Printf.printf "indirect index is %d\n" indirect_index; *)
-        args.(index) <- (indirect_result, indirect_index)
-    | `Relative ->
-        let relative_index : int =
-          Z.to_int (read_memory state (arg_index + index)) + state.relative_base
-        in
-        let relative_result = read_memory state relative_index in
-        args.(index) <- (relative_result, relative_index)
-  in
+  let stop s = {s with stop = true}
 
-  List.iteri ~f:fetch param_specs;
-  args
-
-let rec eval (state : state) : state =
-  let op_code, param_count, param_specs =
-    parse_op_spec (IntMap.find_exn state.data state.index |> Z.to_int)
-  in
-  let args = fetch_args state (state.index + 1) param_count param_specs in
-  let op_fun = make_op_fun op_code in
-  let new_state = op_fun state args in
-  (* Printf.printf "after eval, data has now size: %d\n%!" (IntMap.length state.data); *)
-  if new_state.stop then new_state else eval new_state
-
-let run input data =
-  let mapify (data, index) num =
-    (IntMap.set data ~key:index ~data:num, index + 1)
-  in
-  let initial_state =
+  let create input data =
     {
       stop = false;
       output = [];
-      input = List.map ~f:Z.of_int input;
-      data = List.fold_left ~f:mapify ~init:(IntMap.empty, 0) data |> fst;
+      input;
+      data;
       relative_base = 0;
       index = 0;
     }
-  in
-  eval initial_state
+
+end
+
+
+module Pure_state =
+struct
+  include Common_state
+
+  type t = Z.t IntMap.t state
+
+  let read_memory state address =
+    Option.value ~default:Z.zero (IntMap.find state.data address)
+
+  let write_memory state address value =
+    { state with data = IntMap.set state.data ~key:address ~data:value }
+
+  let create_initial ~input ~initial_data =
+    let mapify (data, index) num =
+      (IntMap.set data ~key:index ~data:num, index + 1)
+    in
+    let data =
+      List.fold_left ~f:mapify ~init:(IntMap.empty, 0) initial_data |> fst
+    in
+    create input data
+end
+
+(* open Pure_state *)
+
+(* type state = t *)
+
+module Intcode_machine (State : State) = struct
+  type nonrec state = State.t
+
+  let make_op_fun (op_code : int) : state -> (Z.t * int) array -> state =
+    let inc_index i args = i + Array.length args + 1 in
+    let skip_args s args = State.with_pc s (inc_index (State.pc s) args) in
+    let mk_arith op state args =
+      let result = op (fst args.(0)) (fst args.(1)) in
+      let update_index = snd args.(2) in
+      let state = State.write_memory state update_index result in
+      State.with_pc state (inc_index (State.pc state) args)
+    in
+    let mk_cond_jmp (op : Z.t -> bool) state args =
+      if op (fst args.(0)) then
+        let alt_value = fst args.(1) |> Z.to_int in
+        State.with_pc state alt_value
+      else State.with_pc state (inc_index (State.pc state) args)
+    in
+    let mk_comp op state args =
+      let result = if op (fst args.(0)) (fst args.(1)) then Z.one else Z.zero in
+      let state = State.write_memory state (snd args.(2)) result in
+      State.with_pc state (inc_index (State.pc state) args)
+    in
+    let mk_adjust_relative_base state args =
+      let rb = State.relative_base state in
+      let state =
+        State.with_relative_base state (rb + (fst args.(0) |> Z.to_int))
+      in
+      skip_args state args
+    in
+    match op_code with
+    | 1 -> mk_arith Z.( + )
+    | 2 -> mk_arith Z.( * )
+    | 3 ->
+        fun state args ->
+          let state, next_input = State.pop_input_exn state in
+          let update_index = snd args.(0) in
+          let state = State.write_memory state update_index next_input in
+          skip_args state args
+    | 4 ->
+        fun state args ->
+          let state = State.push_output state (fst args.(0)) in
+          skip_args state args
+    | 5 -> mk_cond_jmp (Z.Compare.( <> ) Z.zero)
+    | 6 -> mk_cond_jmp (Z.Compare.( = ) Z.zero)
+    | 7 -> mk_comp Z.Compare.( < )
+    | 8 -> mk_comp Z.Compare.( = )
+    | 9 -> mk_adjust_relative_base
+    | 99 -> fun state _args -> State.stop state
+    | bad -> failwith @@ Printf.sprintf "unexpected op_code: %d\n" bad
+
+  let parse_op_spec op_spec =
+    let param_count_of_op = function
+      | 99 -> 0
+      | 3 | 4 -> 1
+      | 5 | 6 -> 2
+      | 1 | 2 | 7 | 8 -> 3
+      | 9 -> 1
+      | _ -> failwith "illegal opcode"
+    in
+    let rec parse_param_mode count code =
+      if count = 0 then []
+      else
+        let rem = code mod 10 in
+        let mode =
+          match rem with
+          | 2 -> `Relative
+          | 1 -> `Immediate
+          | 0 -> `Parameter
+          | _ -> failwith "unexpected parameter mode"
+        in
+        let next = code / 10 in
+        mode :: parse_param_mode (count - 1) next
+    in
+    let op_code = op_spec mod 100 in
+    let param_spec = op_spec / 100 in
+    let param_count = param_count_of_op op_code in
+    let param_spec = parse_param_mode param_count param_spec in
+    (op_code, param_count, param_spec)
+
+  let fetch_args state arg_index param_count param_specs =
+    let args = Array.create ~len:param_count (Z.zero, 0) in
+    let fetch index = function
+      | `Immediate ->
+          let v = State.read_memory state (arg_index + index) in
+          args.(index) <- (v, v |> Z.to_int)
+      | `Parameter ->
+          let indirect_index =
+            Z.to_int (State.read_memory state (arg_index + index))
+          in
+          let indirect_result = State.read_memory state indirect_index in
+          (* Printf.printf "indirect index is %d\n" indirect_index; *)
+          args.(index) <- (indirect_result, indirect_index)
+      | `Relative ->
+          let relative_index : int =
+            Z.to_int (State.read_memory state (arg_index + index))
+            + State.relative_base state
+          in
+          let relative_result = State.read_memory state relative_index in
+          args.(index) <- (relative_result, relative_index)
+    in
+
+    List.iteri ~f:fetch param_specs;
+    args
+
+  let rec eval (state : state) : state =
+    let op_code, param_count, param_specs =
+      parse_op_spec (State.read_memory state (State.pc state) |> Z.to_int)
+    in
+    let args = fetch_args state (State.pc state + 1) param_count param_specs in
+    let op_fun = make_op_fun op_code in
+    let new_state = op_fun state args in
+    (* Printf.printf "after eval, data has now size: %d\n%!" (IntMap.length state.data); *)
+    if State.is_final new_state then new_state else eval new_state
+
+  let run input data =
+    let initial_state =
+      State.create_initial
+        ~input:(List.map ~f:Z.of_int input)
+        ~initial_data:data
+      (* { *)
+      (*   stop = false; *)
+      (*   output = []; *)
+      (*   input = *)
+      (*   data = List.fold_left ~f:mapify ~init:(IntMap.empty, 0) data |> fst; *)
+      (*   relative_base = 0; *)
+      (*   index = 0; *)
+      (* } *)
+    in
+    eval initial_state
+end
+
+module M = Intcode_machine (Pure_state)
+open Pure_state
+open M
 
 let test () =
   let part_two_larger_prog =
